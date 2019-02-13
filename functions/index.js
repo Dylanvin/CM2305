469315e7c24042cd4765a85c14debd99c6ab73da
Dylanvin <dylanvinc@hotmail.co.uk>
2019-02-13 17:14:49 +0000

    added example for extracting vars

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,12 @@ const app = dialogflow({debug: true});
 //  });
 
 app.intent('pls_help', (conv) => {
-    conv.close("Have a good day!");
+    const inString = app.query;
+    conv.close(inString);
   });
+
+app.intent('language', (conv, {language}) => {
+    conv.close(`Wow! I didn't know you knew ${language}!`);   //allows user to extract vars from user input. In this case language
+    });
 
 exports.TutorFunctions = functions.https.onRequest(app);
