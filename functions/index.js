a90c19cdd3479b3b8e4892951732d45fcf25d503
Colbjorn <hdafas@gmail.com>
2019-02-17 22:47:36 +0000

    Added some cool new features to the querying function: -It works That concludes the added features.

--- functions/index.js
+++ functions/index.js
@@ -5,18 +5,22 @@ const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 const { Carousel } = require('actions-on-google');
+const admin = require('firebase-admin');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 // URLs for images used in card rich responses
 const imageUrl = 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png';
 const imageUrl2 = 'https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw';
 const linkUrl = 'https://assistant.google.com/';
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+exports.DBtest = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
 
   function lecture(agent) {
     agent.add("Not sure yet");
@@ -26,7 +30,25 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     const modulueNo = agent.parameters.Modules;
     agent.add(`I don't know who lectures ` + modulueNo);
   }
-  
+
+  function readFromDb (agent) {
+       // Get the database collection 'test col' and document 'test doc'
+       const dialogflowAgentDoc = db.collection('test col').doc('test doc');
+
+       // Get the value of 'TestQuery' in the document and send it to the user
+        return dialogflowAgentDoc.get()
+            .then(doc => {
+                if (!doc.exists) {
+                    agent.add('No data found in the database!');
+                } else {
+                    agent.add(doc.get('TestQuery'));
+                }
+                return Promise.resolve('Read complete');
+            }).catch(() => {
+                agent.add('Error reading entry from the Firestore database.');
+                agent.add('Please add a entry to the database first by saying, "Write <your phrase> to the database"');
+            });
+    }
 
 
 
@@ -34,12 +56,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   let intentMap = new Map();
   intentMap.set('Where_is_lecture', lecture);
   intentMap.set('module_lecturer', module);
+  intentMap.set('db_test', readFromDb);
 
 
   agent.handleRequest(intentMap);
-// Create a query against the collection
-    var queryRef = testCol.where('TestQuery', '==', test);
-    var querySnap = queryRef.get();
-    var result = querySnap.empty;
-    conv.close('Your query is in ' + result);
 });
