66632632488425b1b7c95443558e59cc7e374d23
TheRealDougie1 <DouglasRM@cardiff.ac.uk>
2019-03-06 12:11:59 +0000

    Temp fix for getLecturerInfo.js . No idea if the code itself works, it just no longer throws errors lmao

--- functions/getLecturerInfo.js
+++ functions/getLecturerInfo.js
@@ -5,16 +5,26 @@ function getLecturerEmail(specifiedLecturer) {
 	// Search each document in the Staff table for the correct staff member ( This can be rapidly improved by replacing the document names with distinct values related to the staff and not just auto-generated keys.)
 	db.collection('Staff').get().then( (snapshot) => {
 		// For each loop to loop over each document in the staff table.
+		var found = false;
 		snapshot.docs.forEach(doc => {
 			// Get the name of the staff member of the document.
 			var potentialStaffName = doc.data().Name.toLowerCase();
 			// If the staff name matches the lecture name parsed, stop looping and return the email.
-			if( potentialStaffName == specifiedLecturer ) {
+			if( potentialStaffName === specifiedLecturer ) {
 				// If found, return the lecturer's Email.
-				lecturerEmail == doc.data().Email;
+				found = true;
+				lecturerEmail = doc.data().Email;
 				returnMe(lecturerEmail);
 			}
 		})
+		if (found === true) {
+			return agent.add("")
+		} 
+		else {
+			return agent.add("")
+		}
+	}).catch(() => {
+		agent.add("An internal error occured.");
 	})
 }
 //																											Get Lecturer's Location
@@ -26,12 +36,20 @@ function getLecturerLocation(specifiedLecturer) {
 			// Get the name of the staff member of the document
 			var potentialStaffName = doc.data().Name.toLowerCase();
 			// Compare the parsed name with the name from the document.
-			if ( potentialStaffName == specifiedLecturer ) {
+			if ( potentialStaffName === specifiedLecturer ) {
 				// If found, return the lecturer's location
 				lecturerLoc = doc.data().Location;
 				returnMe(lecturerLoc);
 			}
 		})
+		if (found === true) {
+			return agent.add("")
+		} 
+		else {
+			return agent.add("")
+		}
+	}).catch(() => {
+		agent.add("An internal error occured.");
 	})
 }
 module.exports = {
